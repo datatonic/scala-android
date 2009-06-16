8938f33979f53f1a42230188e46119e28499f3b3
mike.sr <mike.sr@0a078578-5714-11de-b31f-81d54249e7d9>
2009-06-16 23:53:42 +0000

    Fleshed out menus

--- trunk/scala-android/ScalaAndroid/src/scala/android/app/Activity.scala
+++ trunk/scala-android/ScalaAndroid/src/scala/android/app/Activity.scala
@@ -5,15 +5,15 @@ import _root_.scala.android.view._
 trait Activity extends _root_.android.app.Activity{
   import ActivityHelper._
   
-  protected var menu:Menu = null
+  protected var menu:RichMenu = null
   
   override def onCreateOptionsMenu(menu:_root_.android.view.Menu) = {
-	this.menu = new Menu(menu)
+	this.menu = menu
     true
   }
   
   override def onMenuItemSelected(featureId:Int, item:_root_.android.view.MenuItem) = {
-    this.menu(item)
+    this.menu.select(item)
     true
   }
   
@@ -21,11 +21,20 @@ trait Activity extends _root_.android.app.Activity{
 
 object ActivityHelper{
   import _root_.android.view.View.OnClickListener
+  import _root_.android.view.{MenuItem, Menu, SubMenu}
+  import scala.android.view._
   
   implicit def funcToClicker1(f:_root_.android.view.View => Unit):OnClickListener = 
     new OnClickListener(){ def onClick(v:_root_.android.view.View)=f.apply(v)}
   
   implicit def funcToClicker0(f:() => Unit):OnClickListener = 
     new OnClickListener() { def onClick(v:_root_.android.view.View)=f.apply}
+  
+  implicit def menuItemToRichMenuItem(item:MenuItem):RichMenuItem = new RichMenuItem(item)
+  implicit def richMenuItemToMenuItem(item:RichMenuItem):MenuItem = item.base
+  implicit def menuToRichMenu(menu:Menu):RichMenu = new RichMenu(menu)
+  implicit def richMenuToMenu(menu:RichMenu):Menu = menu.base
+  implicit def subMenuToRichSubMenu(menu:SubMenu):RichSubMenu = new scala.android.view.RichSubMenu(menu)
+  implicit def richSubMenuToSubMenu(menu:RichSubMenu):SubMenu = menu.base
 }
 
