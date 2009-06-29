f38b21c0678fab61bbc07736b95148f2328330d0
mike.sr <mike.sr@0a078578-5714-11de-b31f-81d54249e7d9>
2009-06-29 20:34:57 +0000

    Added a couple more implicits for long clicks and focus change events.

--- trunk/scala-android/ScalaAndroid/src/scala/android/app/Activity.scala
+++ trunk/scala-android/ScalaAndroid/src/scala/android/app/Activity.scala
@@ -20,7 +20,7 @@ trait Activity extends _root_.android.app.Activity{
 }
 
 object ActivityHelper{
-  import _root_.android.view.View.OnClickListener
+  import _root_.android.view.View.{OnClickListener,OnLongClickListener, OnFocusChangeListener}
   import _root_.android.view.{MenuItem, Menu, SubMenu}
   import scala.android.view._
   
@@ -29,6 +29,20 @@ object ActivityHelper{
   
   implicit def funcToClicker0(f:() => Unit):OnClickListener = 
     new OnClickListener() { def onClick(v:_root_.android.view.View)=f.apply}
+  
+  implicit def funcToLongClicker0(f:() => Boolean):OnLongClickListener =
+    new OnLongClickListener() { def onLongClick(v:_root_.android.view.View) = f.apply}
+  
+  implicit def funcToLongClicker1(f:_root_.android.view.View => Boolean):OnLongClickListener = 
+    new OnLongClickListener() { def onLongClick(v:_root_.android.view.View) = f.apply(v)}
+  
+  implicit def funcToFocusChanger1(f:Boolean => Unit):OnFocusChangeListener =
+    new OnFocusChangeListener() { def onFocusChange(v:_root_.android.view.View, 
+                                        hasFocus:Boolean) = f.apply(hasFocus)}
+  
+  implicit def funcToFocusChanger2(f:(_root_.android.view.View, Boolean) => Unit):OnFocusChangeListener =
+    new OnFocusChangeListener() { def onFocusChange(v:_root_.android.view.View, 
+                                        hasFocus:Boolean) = f.apply(v,hasFocus)}
   
   implicit def menuItemToRichMenuItem(item:MenuItem):RichMenuItem = new RichMenuItem(item)
   implicit def richMenuItemToMenuItem(item:RichMenuItem):MenuItem = item.base
